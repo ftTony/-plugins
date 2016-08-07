6a6b2891b41f36968adcc8ce86f907d66ddbc01c
tony <172993974@qq.com>
2016-08-07 17:25:56 +0800

    '上传组件'

---
+++ upload/index.js
@@ -0,0 +1,14 @@
+export class Uploader {
+  constructor() {}
+  _init() {}
+  _initInputElement(setting) {}
+  on() {}
+  _callHook(evt, ...args) {}
+  loadFiles(files) {}
+  upload(file) {}
+  chooseFile() {}
+  removeFile() {}
+  clear() {}
+  destroy() {}
+  _post() {}
+}
