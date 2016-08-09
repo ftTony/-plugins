b6ad27977d45c16b47150dd7a71e883386893d4f
tony <172993974@qq.com>
2016-08-09 17:33:56 +0800

    '上传组件'

--- upload/index.js
+++ upload/index.js
@@ -1,14 +1,23 @@
 export class Uploader {
+  //   构造器,new 的时候，合并默认配置
   constructor() {}
+  // 根据配置初始化，绑定事件
   _init() {}
   _initInputElement(setting) {}
+
+  // 绑定金子与触发
   on() {}
   _callHook(evt, ...args) {}
   loadFiles(files) {}
+  // 上传处理
   upload(file) {}
-  chooseFile() {}
+
+  // 交互方法
+  chooseFile(file) {}
+  loadFile(file) {}
   removeFile() {}
   clear() {}
   destroy() {}
+  // 核心ajax发起请求
   _post() {}
 }
