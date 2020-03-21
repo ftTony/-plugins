8ffa17baaeaa25c81d8b699c711862f16c310941
tony <172993974@qq.com>
2020-03-21 14:50:54 +0800

    上传

--- upload/index.js
+++ upload/index.js
@@ -1,3 +1,38 @@
+let uid = 1
+
+const parseError = xhr => {
+    let msg = ''
+    let {
+        responseText,
+        responseType,
+        status,
+        statusText
+    } = xhr
+    if (!responseText && responseType === 'text') {
+        try {
+            msg = JSON.parse(responseText)
+        } catch (error) {
+            msg = responseText
+        }
+    } else {
+        msg = `${status} ${statusText}`
+    }
+
+    const err = new Error(msg)
+    err.status = status
+    return err
+}
+
+const parseSuccess = xhr => {
+    let response = xhr.responseText
+    if (response) {
+        try {
+            return JSON.parse(response)
+        } catch (error) {}
+    }
+    return response
+}
+
 export class Uploader {
     //   构造器,new 的时候，合并默认配置
     constructor(option = {}) {
@@ -46,7 +81,10 @@ export class Uploader {
     chooseFile(file) {}
     loadFile(file) {}
     removeFile() {}
-    clear() {}
+    clear() {
+        this.uploadFiles = []
+        this._callHook('change', this.uploadFiles)
+    }
     destroy() {}
     // 核心ajax发起请求
     _post() {}
